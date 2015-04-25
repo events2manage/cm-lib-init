c86b1e428512322862e8ba3bf7b4445950599bf6
Eni Sinanaj <enisinanaj@outlook.com>
2015-04-26 01:24:17 +0200

    Updates : automation of models and sub library creaiton

--- src/main/java/com/newlinecode/libraryInit/TemplateParser.java
+++ src/main/java/com/newlinecode/libraryInit/TemplateParser.java
@@ -21,81 +21,82 @@ import com.newlinecode.libraryInit.model.TemplateType;
 import com.newlinecode.libraryInit.parser.ModelsParser;
 
 public class TemplateParser implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 	private static final String LIBRARY_TEMPLATE_LOCATION = "template/";
 	private static final String LIBRARY_PATH_TOKEN = "#{LibPath}";
 	private static final String LIBRARY_MODEL_TOKEN = "#{ModelName}";
 	private static final String LIBRARY_MODEL_TBL_TOKEN = "#{ModelTbl}";
 	private static final String LIBRARY_MODEL_KEY_TOKEN = "#{ModelKey}";
-	
+
 	private RootType model;
 	private String libraryPath = "";
 	private String libraryPrefix = "";
-	
+
 	/**
 	 * Parses all the existent templates
 	 * 
 	 * @return operation result
-	 * @throws JAXBException 
+	 * @throws JAXBException
 	 */
 	public boolean parse(String inputModel) throws JAXBException {
-		
-		StringBuilder path = new StringBuilder();
-		
 		JAXBContext jaxbContext = JAXBContext.newInstance(RootType.class);
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		model = (RootType) jaxbUnmarshaller.unmarshal(new File(inputModel));
+		parse((RootType) jaxbUnmarshaller.unmarshal(new File(inputModel)));
+
+		// model.print();
+		return true;
+	}
+
+	public boolean parse(RootType root) {
+		setModel(root);
+		StringBuilder path = new StringBuilder();
 		
-		path.append(model.getProjectPath());
-		model.setProjectPath(normalizePath(path).toString());
+		path.append(getModel().getProjectPath());
+		getModel().setProjectPath(normalizePath(path).toString());
 		determineLibraryPath();
 		buildDirectoryStructure();
-		
+		ModelsParser.createAndParseTemplate(this);
+
 		try {
 			fillTemplates();
 		} catch (Exception e) {
 			System.out.print(e.getMessage());
 			return false;
 		}
-		
-		// model.print();
+
 		return true;
 	}
 
 	public static StringBuilder normalizePath(StringBuilder path) {
-		if(!path.substring(path.length() - 2, path.length()).equals("\\")) {
+		if (!path.substring(path.length() - 2, path.length()).equals("\\")) {
 			path.append("\\");
 		}
-		
+
 		if (!path.toString().contains("libraries")) {
 			path.append("libraries\\");
 		}
-		
+
 		return path;
 	}
-	
+
 	private void buildDirectoryStructure() {
-		createDir(model.getProjectPath() + libraryPath + "\\");
-		
+		createDir(getModel().getProjectPath() + getLibraryPath() + "\\");
+
 		// create helper dir
-		if (model.isHasHelper()) {
-			createDir(model.getProjectPath() + libraryPath + "\\" + "helper\\");
+		if (getModel().isHasHelper()) {
+			createDir(getModel().getProjectPath() + getLibraryPath() + "\\"
+					+ "helper\\");
 		}
-		
+
 		// create helper dir
-		if (model.isHasRules()) {
-			createDir(model.getProjectPath() + libraryPath + "\\" + "rules\\");
-		}
-		
-		// create helper dir
-		if (model.getModels().getModel().size() > 0) {
-			createDir(model.getProjectPath() + libraryPath + "\\" + "model\\");
-			createDir(model.getProjectPath() + libraryPath + "\\" + "collection\\");
+		if (getModel().isHasRules()) {
+			createDir(getModel().getProjectPath() + getLibraryPath() + "\\"
+					+ "rules\\");
 		}
 	}
-	
-	private static boolean createDir(String dir) {
+
+	public static boolean createDir(String dir) {
 		File files = new File(dir.toLowerCase());
 		if (!files.exists()) {
 			if (files.mkdirs()) {
@@ -106,127 +107,211 @@ public class TemplateParser implements Serializable {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 	private boolean fillTemplates() throws IOException {
 		int ok = 1;
 		
-		fillTemplate(TemplateType.CONTROLLER);
-		
+		ModelType m = null;
+		if(model.getModels().getModel().size()>0) {
+			m = model.getModels().getModel().get(0);
+		}
+
+		fillTemplate(TemplateType.CONTROLLER, m);
+
 		// create helper from template
-		if (model.isHasHelper()) {
-			fillTemplate(TemplateType.HELPER);
+		if (getModel().isHasHelper()) {
+			fillTemplate(TemplateType.HELPER, m);
 		}
 
 		// create rules from template
-		if (model.isHasRules()) {
-			fillTemplate(TemplateType.RULES);
+		if (getModel().isHasRules()) {
+			fillTemplate(TemplateType.RULES, m);
 		}
 
-		// create models
-		new ModelsParser(model.getModels()).fillModels();
-		
+        // create helper dir
+        if (m != null) {
+        	fillTemplate(TemplateType.MODEL, m);
+            fillTemplate(TemplateType.COLLECTION, m);
+        }
+
 		return ok != 0;
 	}
-	
-	private boolean fillTemplate(TemplateType templ, ModelType... modelType) throws IOException {
+
+	private boolean fillTemplate(TemplateType templ, ModelType... modelType)
+			throws IOException {
 		ClassLoader classLoader = TemplateParser.class.getClassLoader();
 		File file = null;
-		
+
 		switch (templ) {
-			case RULES:
-				file = new File(classLoader.getResource(LIBRARY_TEMPLATE_LOCATION + TemplateType.RULES.getName()).getFile());
-				break;
-			case HELPER:
-				file = new File(classLoader.getResource(LIBRARY_TEMPLATE_LOCATION + TemplateType.HELPER.getName()).getFile());
-				break;
-			case MODEL:
-				file = new File(classLoader.getResource(LIBRARY_TEMPLATE_LOCATION + TemplateType.MODEL.getName()).getFile());
-				break;
-			case COLLECTION:
-				file = new File(classLoader.getResource(LIBRARY_TEMPLATE_LOCATION + TemplateType.COLLECTION.getName()).getFile());
-				break;
-			case CONTROLLER:
-			default:
-				file = new File(classLoader.getResource(LIBRARY_TEMPLATE_LOCATION + TemplateType.CONTROLLER.getName()).getFile());
-				break;
+		case RULES:
+			file = new File(classLoader.getResource(
+					LIBRARY_TEMPLATE_LOCATION + TemplateType.RULES.getName())
+					.getFile());
+			break;
+		case HELPER:
+			file = new File(classLoader.getResource(
+					LIBRARY_TEMPLATE_LOCATION + TemplateType.HELPER.getName())
+					.getFile());
+			break;
+		case MODEL:
+			file = new File(classLoader.getResource(
+					LIBRARY_TEMPLATE_LOCATION + TemplateType.MODEL.getName())
+					.getFile());
+			break;
+		case COLLECTION:
+			file = new File(classLoader.getResource(
+					LIBRARY_TEMPLATE_LOCATION
+							+ TemplateType.COLLECTION.getName()).getFile());
+			break;
+		case CONTROLLER:
+		default:
+			file = new File(classLoader.getResource(
+					LIBRARY_TEMPLATE_LOCATION
+							+ TemplateType.CONTROLLER.getName()).getFile());
+			break;
 		}
-		
+
 		String content = readFile(file);
-		content = content.replace(LIBRARY_PATH_TOKEN, StringUtils.replace(libraryPrefix, "\\", "_") + StringUtils.replace(libraryPath, "\\", "_"));
+		content = content.replace(LIBRARY_PATH_TOKEN,
+				StringUtils.replace(getLibraryPrefix(), "\\", "_")
+						+ StringUtils.replace(getLibraryPath(), "\\", "_"));
 		if (modelType.length > 0) {
 			// TODO: change into loop
-			content = content.replace(LIBRARY_MODEL_KEY_TOKEN, modelType[0].getModelKey());
-			content = content.replace(LIBRARY_MODEL_TBL_TOKEN, modelType[0].getModelDbName());
+			content = content.replace(LIBRARY_MODEL_KEY_TOKEN,
+					modelType[0].getModelKey());
+			content = content.replace(LIBRARY_MODEL_TBL_TOKEN,
+					modelType[0].getModelDbName());
 			// TODO: First Upper
-			content = content.replace(LIBRARY_MODEL_TOKEN, modelType[0].getModelName().replace("_", "").toLowerCase());
+			content = content.replace(LIBRARY_MODEL_TOKEN, modelType[0]
+					.getModelName().replace("_", "").toLowerCase());
 		}
-		
+
 		return saveContent(templ, content, modelType);
 	}
-	
-	private boolean saveContent(TemplateType templ, String content, ModelType...modelType) {
+
+	private boolean saveContent(TemplateType templ, String content,
+			ModelType... modelType) {
 		File outputFile = null;
-		
+
 		switch (templ) {
-			case RULES: case HELPER: case CONTROLLER: default:
-				outputFile = new File(model.getProjectPath() + libraryPath + "\\" + templ.getName().replace(".tmpl", ""));
-				break;
-			case MODEL:
-				outputFile = new File(model.getProjectPath() + libraryPath + "\\model\\" + modelType[0].getModelName().toLowerCase() + ".php");
-				break;
-			case COLLECTION:
-				outputFile = new File(model.getProjectPath() + libraryPath + "\\collection\\" + modelType[0].getModelName().toLowerCase() + ".php");
-				break;
+		case RULES:
+		case HELPER:
+		case CONTROLLER:
+		default:
+			outputFile = new File(getModel().getProjectPath()
+					+ getLibraryPath() + "\\"
+					+ templ.getName().replace(".tmpl", ""));
+			break;
+		case MODEL:
+			outputFile = new File(getModel().getProjectPath()
+					+ getLibraryPath() + "\\model\\"
+					+ modelType[0].getModelName().toLowerCase() + ".php");
+			break;
+		case COLLECTION:
+			outputFile = new File(getModel().getProjectPath()
+					+ getLibraryPath() + "\\collection\\"
+					+ modelType[0].getModelName().toLowerCase() + ".php");
+			break;
 		}
-		
-        try {
-          BufferedWriter bufferOut = new BufferedWriter(new FileWriter(outputFile));
-          bufferOut.write(content);
-          bufferOut.close();
-        } catch ( IOException e ) {
-           e.printStackTrace();
-           return false;
-        }
-        
-        return true;
+
+		try {
+			BufferedWriter bufferOut = new BufferedWriter(new FileWriter(
+					outputFile));
+			bufferOut.write(content);
+			bufferOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 
 	private static String readFile(File file) throws IOException {
-		DataInputStream dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
-	    try {
-	        long len = file.length();
-	        if (len > Integer.MAX_VALUE) throw new IOException("File "+file.getAbsolutePath()+" too large, was "+len+" bytes.");
-	        byte[] bytes = new byte[(int) len];
-	        dis.readFully(bytes);
-	        return new String(bytes, "UTF-8");
-	    } finally {
-	        dis.close();
-	    }
+		DataInputStream dis = new DataInputStream(new FileInputStream(
+				file.getAbsolutePath()));
+		try {
+			long len = file.length();
+			if (len > Integer.MAX_VALUE)
+				throw new IOException("File " + file.getAbsolutePath()
+						+ " too large, was " + len + " bytes.");
+			byte[] bytes = new byte[(int) len];
+			dis.readFully(bytes);
+			return new String(bytes, "UTF-8");
+		} finally {
+			dis.close();
+		}
 	}
-	
-	private void determineLibraryPath() {		
-		if(model.getProjectPath().contains("libraries")) {
-			String[]pathParts = model.getProjectPath().split("(\\\\|/)");
+
+	private void determineLibraryPath() {
+		if (getModel().getProjectPath().contains("libraries")) {
+			String[] pathParts = getModel().getProjectPath().split("(\\\\|/)");
 			int libIndex = 0;
-			
+
 			for (String part : pathParts) {
 				libIndex++;
 				if ("libraries".equals(part)) {
 					break;
 				}
 			}
-			
-			if( libIndex > 0) {
-				libraryPrefix = StringUtils.join(Arrays.copyOfRange(pathParts.clone(), libIndex++, pathParts.length), "\\");
-				if(!"".equals(libraryPrefix)) {
-					libraryPrefix += "_";
+
+			if (libIndex > 0) {
+				setLibraryPrefix(StringUtils.join(Arrays.copyOfRange(
+						pathParts.clone(), libIndex++, pathParts.length), "\\"));
+				if (!"".equals(getLibraryPrefix())) {
+					setLibraryPrefix(getLibraryPrefix() + "_");
 				}
 			}
 		}
-		
-		libraryPath = model.getControllerName();
+
+		setLibraryPath(getModel().getControllerName());
+	}
+
+	/**
+	 * @return the model
+	 */
+	public RootType getModel() {
+		return model;
+	}
+
+	/**
+	 * @param model
+	 *            the model to set
+	 */
+	public void setModel(RootType model) {
+		this.model = model;
+	}
+
+	/**
+	 * @return the libraryPath
+	 */
+	public String getLibraryPath() {
+		return libraryPath;
+	}
+
+	/**
+	 * @param libraryPath
+	 *            the libraryPath to set
+	 */
+	public void setLibraryPath(String libraryPath) {
+		this.libraryPath = libraryPath;
+	}
+
+	/**
+	 * @return the libraryPrefix
+	 */
+	public String getLibraryPrefix() {
+		return libraryPrefix;
+	}
+
+	/**
+	 * @param libraryPrefix
+	 *            the libraryPrefix to set
+	 */
+	public void setLibraryPrefix(String libraryPrefix) {
+		this.libraryPrefix = libraryPrefix;
 	}
 }
--- src/main/java/com/newlinecode/libraryInit/parser/ModelsParser.java
+++ src/main/java/com/newlinecode/libraryInit/parser/ModelsParser.java
@@ -1,7 +1,11 @@
 package com.newlinecode.libraryInit.parser;
 
+import java.util.List;
+
+import com.newlinecode.libraryInit.TemplateParser;
 import com.newlinecode.libraryInit.model.ModelListType;
 import com.newlinecode.libraryInit.model.ModelType;
+import com.newlinecode.libraryInit.model.RootType;
 
 public class ModelsParser {
 	
@@ -16,13 +20,6 @@ public class ModelsParser {
 	}
 	
 	public void fillModels() {
-		
-		// insert the first model in the root library
-		// create sublibraries for the others
-		
-		for(ModelType model : models.getModel()) {
-			// parse model
-		}
 	}
 
 	/**
@@ -37,5 +34,34 @@ public class ModelsParser {
 	 */
 	public void setModels(ModelListType models) {
 		this.models = models;
+	}
+
+	public static void createAndParseTemplate(TemplateParser context) {
+		// create helper dir
+		RootType model = context.getModel();
+		List<ModelType> modelList = model.getModels().getModel();
+		
+		String basePath = model.getProjectPath() + context.getLibraryPath();
+		if (modelList.size() > 0) {
+			TemplateParser.createDir(basePath + "\\" + "model\\");
+			TemplateParser.createDir(basePath + "\\" + "collection\\");
+		}
+		
+		// from the second model ahead create a sublibrary structure
+		// with helpers controllers rules a model and a collection
+		for(int i = 1; i < modelList.size(); i++) {
+			RootType newRoot = new RootType(modelList.get(i).getModelName(), basePath);
+			newRoot.setHasHelper(true);
+			newRoot.setHasRules(true);
+			newRoot.setControllerName(modelList.get(i).getModelName());
+			newRoot.setProjectPath(basePath);
+			
+			ModelListType newModels = new ModelListType();
+			newModels.getModel().add(modelList.get(i));
+			
+			newRoot.setModels(newModels);
+			TemplateParser tmpl = new TemplateParser();
+			tmpl.parse(newRoot);
+		}
 	}
 }
