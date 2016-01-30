b9a22980276de8b77e7eab435258e311587a374a
Andy Miller <sinanajani@outlook.com>
2016-01-30 01:12:16 +0100

    Updated run.sh for unix based systems. It copies the created library in the project folder (if specified as the second argument) under libraries

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
+#!/bin/sh
+
 java_use 1.7
+echo "Switched to Java 1.7"
+
 mvn -X exec:java -Dexec.mainClass=com.newlinecode.libraryInit.App -Dexec.args="$1"
+
+if [ ! -z "$2" ]; then
+    echo "Moving files to project $2"
+    mv -v libraries/* ~/Documents/htdocs/$2/libraries
+fi
+
